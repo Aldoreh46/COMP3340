c29c97d3e6a43780c1c95c3d200819e2c00c77db
Adam Aldoreh <aldoreh@uwindsor.ca>
2021-01-28 21:15:21 -0500

    Assign 1

--- Assignments/Assignment 1/a1.php
+++ Assignments/Assignment 1/a1.php
@@ -1,1 +1,6 @@
-<?php ?>
+<?php
+ECHO " <br><br><br><br>";
+ECHO "<center><b>Hello!</b><br><br></center>";
+echo "<center>Welcome to My First Assingment<br></center>";
+echo "<body style='background-color:cyan'>";
+?> 
